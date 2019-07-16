00aa7eea607c34a80d506fa4f3ee87e0e54506fb
siteo <phicyanoc@gmail.com>
2019-07-16 23:09:10 +0900

    Other: deinのシンボリックリンク作成処理を削除

--- scripts/nvimrc.bat
+++ scripts/nvimrc.bat
@@ -10,8 +10,6 @@ mkdir %NVIMPATH%
 rem Creates symbolic links
 mklink %NVIMPATH%\init.vim %REPOPATH%\init.vim
 mklink %NVIMPATH%\ginit.vim %REPOPATH%\ginit.vim
-mklink %NVIMPATH%\dein.toml %REPOPATH%\dein.toml
-mklink %NVIMPATH%\dein_lazy.toml %REPOPATH%\dein_lazy.toml
 
 rem Creates nvimenv directories
 mkdir %HOMEPATH%\nvimenv
--- scripts/nvimrc.sh
+++ scripts/nvimrc.sh
@@ -1,4 +1,2 @@
 ln -sf ~/nvimfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/nvimfiles/ginit.vim ~/.config/nvim/ginit.vim
-ln -sf ~/nvimfiles/dein.toml ~/.config/nvim/dein.toml
-ln -sf ~/nvimfiles/dein_lazy.toml ~/.config/nvim/dein_lazy.toml

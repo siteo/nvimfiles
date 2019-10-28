b4a245d46fdb6c576e366207a4ddcfb814098bbe
siteyo <phicyanoc@gmail.com>
2019-10-28 22:44:46 +0900

    Update

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 mkdir ~/.config/nvim
 mkdir ~/nvimbackup
 mkdir ~/nvimundo
+mkdir ~/.nvim
 
 ln -sf ~/nvimfiles/init.vim ~/.config/nvim/init.vim
 ln -sf ~/nvimfiles/ginit.vim ~/.config/nvim/ginit.vim
